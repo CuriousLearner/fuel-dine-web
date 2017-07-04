acf2934c4a1fcc6b245c5593533e0b4826d62b78
CuriousLearner <sanyam.khurana01@gmail.com>
2017-07-04 11:25:37 +0530

    Add permissions and docstrings to views

--- fuel_dine/restaurants/views.py
+++ fuel_dine/restaurants/views.py
@@ -4,12 +4,14 @@ from __future__ import unicode_literals
 from django.views.generic import DetailView
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListCreateAPIView
 from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 from .models import Restaurant, Review, Comment
@@ -20,39 +22,70 @@ from .forms import ReviewForm, CommentForm
 
 
 class RestaurantView(ListCreateAPIView):
+    """Restaurant Listing API for home page.
+
+    Renders restaurants/index.html by default. Also support JSON serialization.
+    """
     queryset = Restaurant.objects.filter(is_active=True)
     serializer_class = RestaurantSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     renderer_classes = (TemplateHTMLRenderer, JSONRenderer)
     template_name = "restaurants/index.html"
 
 
 class ReviewView(ListCreateAPIView):
+    """Review Listing API
+    """
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class RestaurantDetailView(DetailView):
+    """Restaurant Detail API to be displayed on Restaurant Display page
+    along with reviews and comments.
+    """
     model = Restaurant
+    permission_classes = (IsAuthenticated,)
 
 
 class RestaurantCreateView(CreateAPIView):
+    """API to create restaurant
+    """
     model = Restaurant
     serializer_class = RestaurantSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class ReviewCreateView(CreateAPIView):
+    """API to post new Reviews for Restaurants
+    """
     model = Review
     serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class CommentCreateView(CreateAPIView):
+    """API to post new comments on Reviews
+    """
     model = Comment
     serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 # All FBVs goes here
 
+@login_required()
+@permission_classes(IsAuthenticated,)
 def add_review_form(request, pk):
+    """View for rendering form for adding review to a particular restaurant.
+    Initial form contains Review text as "I like ..."
+
+    :param request: HTTPRequest Object
+    :param pk: Primary key of Restaurant to post review about.
+    :return: HttpResponseRedirect to /thanks/ on successful posting.
+    If Review text is empty, return the Form with error.
+    """
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = ReviewForm(request.POST)
@@ -75,7 +108,17 @@ def add_review_form(request, pk):
                   {'form': form, 'restaurant': pk})
 
 
+@api_view(['GET', 'POST'])
+@permission_classes(IsAuthenticated,)
 def add_comment_form(request, pk):
+    """View for rendering form for adding comment to a particular review.
+    Initial form contains Comment text as "I like ..."
+
+    :param request: HTTPRequest Object
+    :param pk: Primary key of Review object to post comment about.
+    :return: HttpResponseRedirect to /thanks/ on successful posting.
+    If Comment text is empty, return the Form with error.
+    """
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = CommentForm(request.POST)
@@ -99,7 +142,24 @@ def add_comment_form(request, pk):
 
 
 @api_view(['POST'])
+@permission_classes(IsAuthenticated,)
 def vote_for_restaurant(request, pk, action):
+    """API for up vote or down vote a particular Restaurant.
+
+    :param request: HttpRequest Object.
+    :param pk: Primary key of Restaurant.
+    :param action: either `up` for up voting or `down` for down voting the
+    restaurant.
+    :return: Return 201 status on successful vote cast & JSON Response as:
+    {
+        'data': 'Vote casted successfully'
+    }
+
+    Return 400 status on Unknown action & JSON Response as:
+    {
+        'error': 'Unknown action for casting vote'
+    }
+    """
     user_id = request.user.profile.id
     try:
         restaurant = Restaurant.objects.get(id=pk)
